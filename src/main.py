bf855000d91ce013df4176a3664213f990628d2b
Michael Thomas <michaeljthomas97@yahoo.com>
2023-07-04 11:32:03 +0100

    changes

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ import glob
 logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
 
 failed_images_path = f'{os.getcwd()}/failed_images'
+completed_image_path = f'{os.getcwd()}/completed_images'
 
 images_folder = f'{os.getcwd()}/images'
 
@@ -38,14 +39,17 @@ for image_file in image_files:
 
         # Cut out bottom right part of the card to get set name and code
         height, width, _ = clean_img.shape
-        start_row, start_col = int(height * .55), int(width * .38)
+        start_row, start_col = int(height * .47), int(width * .375)
         set_name_roi = clean_img[start_row:, start_col:]
+        #cv2.imwrite(f'{os.getcwd()}/setnameimage.jpg', set_name_roi) #Uncomment to see what part of the image we're cutting
 
         # Perform OCR on the set name region of interest (ROI)
         ocr_result = pytesseract.image_to_string(set_name_roi)
 
-        # Split the result into lines
-        lines = [line.strip() for line in ocr_result.split('\n') if line.strip() != '']
+        # Split the result into lines, ignoring lines shorter than 4 characters or starting with 'EN'
+        lines = [line.strip() for line in ocr_result.split('\n') if line.strip() != '' and len(line) > 4 and not line.startswith('EN')]
+
+        logging.info(f'Extracted data - {lines}')
 
         if len(lines) < 2:
             logging.info(f'Failed to extract code and set name from {image_file}')
@@ -68,6 +72,8 @@ for image_file in image_files:
         logging.info('Codes match. Writing to file...')
         with open('output.txt', 'a', encoding='utf-8') as f:
             f.write(f'{code},{set_name}\n')
+
+        shutil.move(image_file, f'{completed_image_path}/{os.path.basename(image_file)}')  # Move to failed images folder
 
     except Exception as e:
         logging.error(f'An error occurred processing {image_file}: {e}')
